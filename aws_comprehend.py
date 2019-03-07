4c78d57e1cbfb04dea976ede3bdbd18cb9be66f7
Nathaniel Ng <nathanielng@users.noreply.github.com>
2019-03-07 10:41:47 +0000

    Add AWS Comprehend

---
+++ aws_comprehend.py
@@ -0,0 +1,93 @@
+import argparse
+import boto3
+import botocore
+import json
+import sys
+
+
+class LanguageEngine:
+    """A class for Natural Language Processing"""
+
+
+    def __init__(self, region_name, language='en'):
+        self._region = region_name
+        self._language_code = language
+        self._comprehend = boto3.client(
+            service_name='comprehend',
+            region_name=region_name)
+
+
+    def get_dominant_language(self, text):
+        try:
+            json_obj = self._comprehend.detect_dominant_language(
+                Text=text)
+            result = json.dumps(json_obj['Languages'], sort_keys=True, indent=4)
+        except (Exception, botocore.exceptions.EndpointConnectionError) as e:
+            result = str(e)
+        finally:
+            return result
+
+
+    def get_key_phrases(self, text):
+        try:
+            json_obj = self._comprehend.detect_key_phrases(
+                Text=text,
+                LanguageCode=self._language_code)
+            result = json.dumps(json_obj['KeyPhrases'], sort_keys=True, indent=4)
+        except Exception as e:
+            result = e
+        finally:
+            return result
+
+
+class LanguageEngineMedical:
+    """A class for Natural Language Processing of Medical Information"""
+
+
+    def __init__(self, region_name, language='en'):
+        self._region = region_name
+        self._language_code = language
+        self._comprehend = boto3.client(
+            service_name='comprehendmedical',
+            region_name=region_name)
+
+
+    def get_entities(self, text):
+        try:
+            result = self._comprehend.detect_entities(
+                Text=text)
+            entities = result['Entities']
+            txt = ''
+            for entity in entities:
+                txt += f'{json.dumps(entity, sort_keys=True, indent=4)}\n'
+        except Exception as e:
+            txt = e
+        finally:
+            return txt.strip()
+
+
+AWS_REGION = 'ap-southeast-1'
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--text', default='')
+    args = parser.parse_args()
+
+    if len(args.text) == 0:
+        print(f'Usage: {sys.argv[0]} --text "text to analyze"')
+        exit()
+
+    LE = LanguageEngine(AWS_REGION)
+    txt = LE.get_dominant_language(args.text)
+    print('----- Dominant Language -----')
+    print(txt)
+
+    txt = LE.get_key_phrases(args.text)
+    print('----- Key Phrases -----')
+    print(txt)
+
+    LEM = LanguageEngineMedical('us-west-2')
+    txt = LEM.get_entities(args.text)
+    print('----- Entities (Medical) -----')
+    print(txt)
+
--- bot.py
+++ bot.py
@@ -1,17 +1,116 @@
-import boto3
+import argparse
+import aws_comprehend as ac
 import json
 import os
-import pandas as pd
-import re
+import slackclient
 import time
-from slackclient import SlackClient
+
+
+class MySlackClass:
+    """A class for handling Slack Calls"""
+
+
+    def __init__(self, token=None):
+        if token is None:
+            if 'SLACK_BOT_TOKEN' not in os.environ:
+                print('SLACK_BOT_TOKEN has not been defined as an environment variable')
+                quit()
+            self._token = os.environ.get('SLACK_BOT_TOKEN')
+        else:
+            self._token = token
+        self._slack_client = slackclient.SlackClient(self._token)
+
+
+    def api_call(self, *args, **kwargs):
+        return self._slack_client.api_call(*args, **kwargs)
+
+
+    def bot_userid(self):
+        return self.api_call('auth.test')['user_id']
+
+
+    def get_channels(self):
+        result = self.api_call("channels.list")
+        if result.get('ok'):
+            return result['channels']
+
+
+    def list_channels(self):
+        channels = self.get_channels()
+        for channel in channels:
+            # print(channel)
+            print('{id} "{name}": Purpose - {purpose}'.format(**channel))
+
+
+    def send_message(self, channel_id, message):
+        result = self._slack_client.api_call(
+            "chat.postMessage",
+            channel=channel_id,
+            text=message,
+            username='pythonbot',
+            icon_emoji=':robot_face:'
+        )
+        return result
+
+
+    def parse_commands(self, events, LEM=None):
+        for event in events:
+            if event['type'] == 'message' and not 'subtype' in event:
+                user_id = event['user']
+                msg = event['text']
+                channel = event['channel']
+                print(f'User: {user_id}@{channel}, msg="{msg}"')
+                if LEM is not None:
+                    txt = LEM.get_entities(msg)
+                    print(f'Text: {txt}')
+                    self.send_message(channel, txt)
+
+
+    def run(self, LEM=None, delay=1):
+        if self._slack_client.rtm_connect(with_team_state=False) is False:
+            print('Connection failed')
+            quit()
+
+        bot_id = self.bot_userid()
+        print(f'Bot (ID={bot_id}) is now running ', end='')
+        print(f'on `slackclient` version {slackclient.version.__version__}')
+        while True:
+            events = self._slack_client.rtm_read()
+            print(events)
+            if len(events) > 0:
+                self.parse_commands(events, LEM)
+            time.sleep(delay)
 
 
 if __name__ == "__main__":
-    if 'SLACK_BOT_TOKEN' not in os.environ:
-        print('SLACK_BOT_TOKEN has not been defined as an environment variable')
-        quit()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--api_test', action='store_true')
+    parser.add_argument('--channels', action='store_true')
+    parser.add_argument('--message', default='')
+    parser.add_argument('--run', action='store_true')
+    parser.add_argument('--userid', action='store_true')
+    args = parser.parse_args()
 
-    token = os.environ.get('SLACK_BOT_TOKEN')
-    slack_client = SlackClient(token)
+    SC = MySlackClass()
+
+    if args.api_test is True:
+        # python3 bot.py --api_test
+        result = SC.api_call('api.test')
+        txt = json.dumps(result, indent=4)
+        print(txt)
+    elif args.channels is True:
+        # python3 bot.py --channels
+        txt = SC.list_channels()
+        print(txt)
+    elif len(args.message) > 0:
+        # python3 bot.py --message "Hello"
+        my_channel = 'CGRBJFKPH'
+        result = SC.send_message(my_channel, args.message)
+    elif args.userid is True:
+        # python3 bot.py --userid
+        print(f'Bot userid = {SC.bot_userid()}')
+    elif args.run is True:
+        # python3 bot.py --run
+        LEM = ac.LanguageEngineMedical('us-west-2')
+        SC.run(LEM)
 
